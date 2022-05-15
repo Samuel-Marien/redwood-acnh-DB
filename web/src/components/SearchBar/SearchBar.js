2f0f9f146e0621de72a8c36714a05300e149fd9c
Samuel <timeuss.sam@gmail.com>
2022-05-15 16:37:13 +0200

    add design on search bar & refactor compo to be reusable

---
+++ web/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,53 @@
+import { Link, routes } from '@redwoodjs/router'
+import { Form, TextField, Submit } from '@redwoodjs/forms'
+import { GiArchiveResearch } from 'react-icons/gi'
+import { BiReset } from 'react-icons/bi'
+
+const SearchBar = (props) => {
+  const { state, onSubmit, onClick, name, placeholder, ressourcesName } = props
+  return (
+    <div className="mb-5 flex flex-col justify-center items-center">
+      <img src="images/illustration-cut.jpg" alt="hero" />
+      <h1 className="text-4xl mb-3 font-inika font-bold text-myBrown-100">
+        {ressourcesName} Ressources
+      </h1>
+      <div className="flex justify-center items-center">
+        {state.id ? (
+          <div>
+            <Link to={routes.fishsPage()}>
+              <button
+                onClick={onClick}
+                className="text-3xl mr-2 text-myPink-100 hover:text-myPink-200 transition-colors hover:animate-pulse"
+              >
+                <BiReset />
+              </button>
+            </Link>
+          </div>
+        ) : null}
+        <Form onSubmit={onSubmit} style={{ fontSize: '2rem' }}>
+          <div className="flex items-center">
+            <TextField
+              name={name}
+              placeholder={placeholder}
+              validation={{ required: true }}
+              className="border border-myYellow-100  h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+            />
+            {state.id ? (
+              <Link
+                to={routes.details({ id: state.id })}
+                style={{ transform: 'translate(-120%)' }}
+                className="mt-1"
+              >
+                <Submit className=" text-myGreen-200">
+                  <GiArchiveResearch />
+                </Submit>
+              </Link>
+            ) : null}
+          </div>
+        </Form>
+      </div>
+    </div>
+  )
+}
+
+export default SearchBar
--- web/src/pages/FishsPagePage/FishsPagePage.js
+++ web/src/pages/FishsPagePage/FishsPagePage.js
@@ -1,9 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { MetaTags } from '@redwoodjs/web'
 import { Link, routes } from '@redwoodjs/router'
-import { Form, TextField, Submit } from '@redwoodjs/forms'
 import { useState, useEffect } from 'react'
 import CardsDisplayer from 'src/components/CardsDisplayer/CardsDisplayer'
+import SearchBar from 'src/components/SearchBar/SearchBar'
 
 const FishsPagePage = () => {
   const [state, setState] = useState([])
@@ -20,9 +20,9 @@ const FishsPagePage = () => {
   useEffect(() => {
     try {
       for (let item of Object.getOwnPropertyNames(stateAll)) {
-        const fishName = stateAll[item]
+        const itemName = stateAll[item]
         myArray.push({
-          fr: fishName.name['name-EUfr'],
+          fr: itemName.name['name-EUfr'],
           en: item,
         })
       }
@@ -52,50 +52,34 @@ const FishsPagePage = () => {
     <>
       <MetaTags title="FishsPage" description="FishsPage page" />
 
-      <Form onSubmit={onSubmit} style={{ fontSize: '2rem' }}>
-        <TextField
-          name="fishname"
-          placeholder="Fish Name"
-          validation={{ required: true }}
-        />
-        {state.id ? (
-          <div>
-            <Link to={routes.details({ id: state.id })}>
-              <Submit className="text-sm border p-1 shadow hover:shadow-none">
-                See details
-              </Submit>
-            </Link>
-            <Link to={routes.fishsPage()}>
-              <button
-                onClick={() => setState([])}
-                className="text-sm border p-1 shadow hover:shadow-none"
-              >
-                Reset
-              </button>
-            </Link>
-          </div>
-        ) : null}
-        {state.id ? (
+      <SearchBar
+        name="fishname"
+        placeholder="Fish name..."
+        onSubmit={onSubmit}
+        onClick={() => setState([])}
+        state={state}
+        ressourcesName="Fishs"
+      />
+      {state.id ? (
+        <div className="border p-2 m-2 shadow w-48">
           <Link to={routes.details({ id: state.id })}>
-            <div className="border p-2 m-2 shadow w-48">
-              <div className="flex justify-between items-center">
-                <p>{state.name['name-EUfr']}</p>
-                <p className="text-xs">#{state.id}</p>
-              </div>
-              <img src={state.icon_uri} alt={state.name['name-EUfr']} />
-              <p className="text-center mb-2">{state.price} $</p>
-              <div className="flex justify-between">
-                <p className="w-1/3 flex justify-center px-1 py-1 bg-yellow-500 text-white text-sm font-medium rounded-full">
-                  {state.availability.location}
-                </p>
-                <p className=" flex justify-center px-1 py-1 bg-red-500 text-white text-sm font-medium rounded-full">
-                  {state.availability.rarity}
-                </p>
-              </div>
+            <div className="flex justify-between items-center">
+              <p>{state.name['name-EUfr']}</p>
+              <p className="text-xs">#{state.id}</p>
+            </div>
+            <img src={state.icon_uri} alt={state.name['name-EUfr']} />
+            <p className="text-center mb-2">{state.price} $</p>
+            <div className="flex justify-between">
+              <p className="w-1/3 flex justify-center px-1 py-1 bg-yellow-500 text-white text-sm font-medium rounded-full">
+                {state.availability.location}
+              </p>
+              <p className=" flex justify-center px-1 py-1 bg-red-500 text-white text-sm font-medium rounded-full">
+                {state.availability.rarity}
+              </p>
             </div>
           </Link>
-        ) : null}
-      </Form>
+        </div>
+      ) : null}
       <CardsDisplayer cardsDatas={stateAll} />
     </>
   )
